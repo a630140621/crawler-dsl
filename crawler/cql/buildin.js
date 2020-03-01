2f559bd7749c6b401664db19053809611b07b0c0
lovecrazy <630140621@qq.com>
2020-03-01 17:31:46 +0800

    统一 buildin 函数的逻辑

--- crawler/cql/buildin.js
+++ crawler/cql/buildin.js
@@ -14,21 +14,36 @@ function raw(string) {
     return ret;
 }
 
-function wrapper($, script) {
+
+// script: "text($('li'))"
+function wrapper(_$, script) {
     // 内置函数
+    function $(selector) {
+        let node = _$(selector);
+        node.$ = true; // 后续调用 attr, text, html 等内置函数 时返回字符串
+        return node;
+    }
+
+    function $$(selector) {
+        return _$(selector);
+    }
+
     function css(selector) {
-        return $(selector);
+        return $$(selector);
     }
 
     // 获取节点属性
     function attr($_, attribute) {
-        let ret = [];
-        $_.each(function (i, elem) {
-            ret.push($(this).attr(attribute));
-        });
+        debug(`run buildin func attr to get attribute ${attribute}`);
+        if ($_.$) return $_.eq(0).attr(attribute) || "";
+        else {
+            let ret = [];
+            $_.each(function (i, elem) {
+                ret.push(_$(this).attr(attribute) || "");
+            });
 
-        debug(`run buildin func attr ret = ${ret}`);
-        return ret;
+            return ret;
+        }
     }
 
     function href($_) {
@@ -39,55 +54,70 @@ function wrapper($, script) {
         return attr($_, "src");
     }
 
-    // @return ["text"]
+    // @return "text" or ["text"]
     function text($_) {
-        let ret = [];
-        // 虽然下述是回调函数形式，但是是同步执行的
-        $_.each(function (i, elem) {
-            ret.push($(this).text().trim());
-        });
+        debug(`run buildin func text`);
+        if ($_.$) return $_.eq(0).text().trim();
+        else {
+            let ret = [];
+            // 虽然下述是回调函数形式，但是是同步执行的
+            $_.each(function (i, elem) {
+                ret.push(_$(this).text().trim());
+            });
 
-        debug(`run buildin func text ret = ${ret}`);
-        return ret;
+            return ret;
+        }
     }
 
-    // @return ["html"]
+    // @return "html", ["html"]
     function html($_) {
-        let ret = [];
-        // 虽然下述是回调函数形式，但是是同步执行的
-        $_.each(function (i, elem) {
-            ret.push($(this).toString().trim());
-        });
+        debug(`run buildin func html`);
+        if ($_.$) return $_.eq(0).toString().trim();
+        else {
+            let ret = [];
+            // 虽然下述是回调函数形式，但是是同步执行的
+            $_.each(function (i, elem) {
+                ret.push(_$(this).toString().trim());
+            });
 
-        debug(`run buildin func html ret = ${ret}`);
-        return ret;
+            return ret;
+        }
     }
 
     /**
      * 使用正则表达式匹配文本内容，
      * 如果没传第二个参数，则全文匹配，并返回匹配到的第一个值
-     * 如果传入第二个参数，则第二个参数必须是数组，对于每个元素执行一次匹配，返回匹配到的第一个值，若没匹配到则返回 ""
+     * 如果传入第二个参数：
+     *  1. 第二个参数是字符串数组，对数组中的每个元素执行一次匹配，返回匹配到的第一个值，若没匹配到则返回 []
+     *  2. 第二个参数是 字符串，对该字符串执行一次匹配，返回匹配到的第一个值，若没匹配到则返回 ""
      * @param {RegExp} regex 正则表达式
      * @param {[String]} texts 
-     * 
-     * @return [""]
      */
     function regex(regex, texts) {
-        if (!texts) texts = [$.html()];
-        let ret = [];
-        for (let text of texts) {
+        debug(`run buildin func regex on texts = ${texts}`);
+        if (typeof texts === "string" || !texts) {
+            let text;
+            if (typeof texts === "string") text = texts;
+            else text = _$.html();
             let match = text.match(regex);
-            if (!match) ret.push("");
-            else ret.push(match[0].trim());
+            if (!match) return "";
+            return match[0].trim();
+        } else {
+            let ret = [];
+            for (let text of texts) {
+                let match = text.match(regex);
+                if (!match) ret.push("");
+                else ret.push(match[0].trim());
+            }
+            return ret;
         }
-
-        debug(`run buildin func regex ret = ${ret}`);
-        return ret;
     }
 
 
     function runCodeWithBuildIn(func_str) {
         return Function(`return (${func_str})`)()(
+            $,
+            $$,
             css,
             text,
             regex,
@@ -98,7 +128,7 @@ function wrapper($, script) {
         );
     }
 
-    return runCodeWithBuildIn(`function(css, text, regex, html, attr, href, src){ return ${raw(script)} }`);
+    return runCodeWithBuildIn(`function($, $$, css, text, regex, html, attr, href, src){ return ${raw(script)} }`);
 }
 
 /**
--- test/buildin.test.js
+++ test/buildin.test.js
@@ -10,8 +10,33 @@ const $ = cheerio.load(html, {
     decodeEntities: false
 });
 
+const fruits = cheerio.load(`
+<ul id="fruits">
+    <li class="apple">Apple</li>
+    <li class="orange">Orange</li>
+    <li class="pear">Pear</li>
+</ul>`);
 
 describe("buildin", () => {
+    it("buildin.$ with text() should return first node innerText", () => {
+        expect(buildin.evalBuildInScript(fruits, "text($('li'))")).to.be.a("string").that.equal("Apple");
+    });
+    it("buildin.$ with text() should return first node innerText", () => {
+        expect(buildin.evalBuildInScript(fruits, "text($('ul'))")).to.be.a("string").that.contain("Apple").contain("Orange").contain("Pear");
+    });
+    it("buildin.$ with text() should return '' if not match", () => {
+        expect(buildin.evalBuildInScript(fruits, "text($('none'))")).to.be.a("string").that.empty;
+    });
+
+    it("buildin.$$ with text() should return array which have each node innerText", () => {
+        expect(buildin.evalBuildInScript(fruits, "text($$('li'))")).to.be.an("array")
+            .that.have.length(3)
+            .and.include("Apple").include("Orange").include("Pear");
+    });
+    it("buildin.$$ with text() should return [] if not match", () => {
+        expect(buildin.evalBuildInScript(fruits, "text($$('none'))")).to.be.an("array").that.empty;
+    });
+
     it("extract title use text() and css()", () => {
         let script = "text(css('h1'))";
         expect(buildin.evalBuildInScript($, script)).to.be.an("array").have.length(1).and.that.include("微视频 | 愿得此身长报国");
@@ -20,22 +45,34 @@ describe("buildin", () => {
         let script = "text(css('.does .not .exist'))";
         expect(buildin.evalBuildInScript($, script)).to.be.an("array").that.empty;
     });
-    it("extract pubdate use regex with two params", () => {
+
+
+    it("buildin.regex with $", () => {
+        let script = "regex('\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}', text($('#epContentLeft')))";
+        expect(buildin.evalBuildInScript($, script)).to.be.an("string").that.equal("2020-02-17 13:22:54");
+    });
+    it("buildin.regex with $", () => {
+        let script = "regex('\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{3}', text($('#epContentLeft')))";
+        expect(buildin.evalBuildInScript($, script)).to.be.an("string").that.equal("");
+    });
+    it("buildin.regex extract pubdate use regex with two params", () => {
         let script = "regex('\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}', text(css('#epContentLeft')))";
         expect(buildin.evalBuildInScript($, script)).to.be.an("array").have.length(1).and.that.include("2020-02-17 13:22:54");
     });
-    it("extract pubdate in specify area wich does not match", () => {
+    it("buildin.regex extract pubdate in specify area wich does not match", () => {
         let script = "regex('\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}', text(css('#epContentRight')))";
         expect(buildin.evalBuildInScript($, script)).to.be.an("array").have.length(1).and.that.include("");
     });
-    it("extract pubdate use regex with two params which does not match", () => {
+    it("buildin.regex extract pubdate use regex with two params which does not match", () => {
         let script = "regex('这是随便写的一个正则字符串...', text(css('#epContentLeft')))";
         expect(buildin.evalBuildInScript($, script)).to.be.an("array").have.length(1).and.that.include("");
     });
-    it("extract pubdate use regex with one params", () => {
+    it("buildin.regex extract pubdate use regex with one params", () => {
         let script = "regex(/\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}/ig)";
-        expect(buildin.evalBuildInScript($, script)).to.be.an("array").have.length(1).and.that.include("2020-02-17 13:22:54");
+        expect(buildin.evalBuildInScript($, script)).to.be.an("string").and.that.equal("2020-02-17 13:22:54");
     });
+
+
     it("extract origin  use text() and css()", () => {
         let script = "text(css('#ne_article_source'))";
         expect(buildin.evalBuildInScript($, script)).to.be.an("array").have.length(1).and.that.include("央视新闻客户端");
@@ -44,12 +81,69 @@ describe("buildin", () => {
         let script = "html(css('h1'))";
         expect(buildin.evalBuildInScript($, script)).to.be.an("array").have.length(1).and.that.include("<h1>微视频 | 愿得此身长报国</h1>");
     });
-    it("extract video src use src() and css()", () => {
+    it("extract title html use html() and $$()", () => {
+        let script = "html($$('h1'))";
+        expect(buildin.evalBuildInScript($, script)).to.be.an("array").have.length(1).and.that.include("<h1>微视频 | 愿得此身长报国</h1>");
+    });
+    it("extract title html use html() and $()", () => {
+        let script = "html($('h1'))";
+        expect(buildin.evalBuildInScript($, script)).to.be.an("string").that.equal("<h1>微视频 | 愿得此身长报国</h1>");
+    });
+
+    it("buildin.src extract first <img> src use src() and $()", () => {
+        let script = "src($('.post_body li img'))";
+        let html = `
+        <ul class="post_body">
+            <li><img src="http://www.demo.org/test1"></li>
+            <li><img src="http://www.demo.org/test2"></li>
+        </ul>`;
+        expect(buildin.evalBuildInScript(cheerio.load(html), script)).to.be.an("string").that.equal("http://www.demo.org/test1");
+    });
+    it("buildin.attr extract first <img> src use attr() and $()", () => {
+        let script = "attr($$('.post_body li img'), 'src')";
+        let html = `
+        <ul class="post_body">
+            <li><img src="http://www.demo.org/test1"></li>
+            <li><img src="http://www.demo.org/test2"></li>
+        </ul>`;
+        expect(buildin.evalBuildInScript(cheerio.load(html), script)).to.be.an("array")
+            .that.have.length(2)
+            .and.include("http://www.demo.org/test1").include("http://www.demo.org/test2");
+    });
+    it("buildin.src extract <img> src use src() and css()", () => {
         let script = "src(css('.post_body p img'))";
         expect(buildin.evalBuildInScript($, script)).to.be.an("array").have.length(6);
     });
-    it("extract video src use attr() and css()", () => {
+    it("buildin.src extract <img> src use src() and $$()", () => {
+        let script = "src($$('.post_body p img'))";
+        expect(buildin.evalBuildInScript($, script)).to.be.an("array").have.length(6);
+    });
+    it("buildin.attr extract <img> src use attr() and css()", () => {
         let script = "attr(css('.post_body p img'), 'src')";
         expect(buildin.evalBuildInScript($, script)).to.be.an("array").have.length(6);
+    });
+    it("buildin.attr extract <img> src use attr() and $$()", () => {
+        let script = "attr($$('.post_body p img'), 'src')";
+        expect(buildin.evalBuildInScript($, script)).to.be.an("array").have.length(6);
+    });
+    it("buildin.href with buildin.$ should return string", () => {
+        let script = "href($('a'))";
+        expect(buildin.evalBuildInScript(cheerio.load("<a href='http://www.baidu.com'>百度</a>"), script))
+            .to.be.a("string").that.equal("http://www.baidu.com");
+    });
+    it("buildin.href with buildin.$ should return string", () => {
+        let script = "href($('div'))";
+        expect(buildin.evalBuildInScript(cheerio.load("<div>no href</div>"), script))
+            .to.be.a("string").that.equal("");
+    });
+    it("buildin.href with buildin.$$ should return [string]", () => {
+        let script = "href($$('a'))";
+        expect(buildin.evalBuildInScript(cheerio.load("<a href='http://www.baidu.com'>百度</a>"), script))
+            .to.be.an("array").that.have.length(1).and.include("http://www.baidu.com");
+    });
+    it("buildin.href with buildin.$$ should return ['']", () => {
+        let script = "href($$('div'))";
+        expect(buildin.evalBuildInScript(cheerio.load("<div>百度</div>"), script))
+            .to.be.an("array").that.have.length(1).and.include("");
     });
 });
